1aa091d9664ffa985ace1793df424261d3545ed2
Ungureanu Cosmin <ucosmin194@gmail.com>
2022-09-26 17:40:59 +0300

    Post list

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,27 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+    public BlogController(PostRepository postRepository){
+
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
+        List<Post> posts = PostRepository.getAllPosts();
+        modelMap.put("posts", posts);
         modelMap.put("title", "Blog Post 1");
         return "home";
     }
 
 }
+
 
